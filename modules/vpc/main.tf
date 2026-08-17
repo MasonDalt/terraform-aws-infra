dc9f735dd473b93f4ab6cb19828e93247b671c20
MasonDalt <kraevv316@gmail.com>
2026-08-17 23:16:41 +0400

    feat(vpc): add VPC module with subnet, IGW and route table

---
+++ modules/vpc/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "dev" {
+    cidr_block = var.vpccidr
+    tags = {
+      Name = "tors"
+    }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.dev.id
+    cidr_block = var.subcidr
+    availability_zone = var.zone
+    tags = {
+      Name = "sub"
+    }
+}
+
+resource "aws_internet_gateway" "int" {
+    vpc_id = aws_vpc.dev.id
+    tags = {
+      Name = "inget"
+    }
+}
+
+resource "aws_route_table" "rou" {
+    vpc_id = aws_vpc.dev.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.int.id
+    }
+}
+
+resource "aws_route_table_association" "soc" {
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.rou.id
+}
